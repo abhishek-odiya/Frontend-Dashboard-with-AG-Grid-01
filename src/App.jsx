e4813eb030afa1ff232f448278b9c69cf1201a18
abhishek-odiya <abhishekodiya@outlook.com>
2025-11-29 00:45:49 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import "./index.css";
+import EmployeeGrid from "./components/EmployeeGrid";
+
+export default function App() {
+    return (
+        <EmployeeGrid />
+    );
+}
---
+++ src/components/EmployeeGrid.jsx
@@ -0,0 +1,141 @@
+import { useState, useMemo, useRef } from "react";
+import { AgGridReact } from "ag-grid-react";
+import { ModuleRegistry, AllCommunityModule } from "ag-grid-community";
+import employees from "../data/assessment_data.json";
+
+ModuleRegistry.registerModules([AllCommunityModule]);
+
+export default function EmployeeGrid() {
+    const [filterText, setFilterText] = useState("");
+    const gridRef = useRef();
+
+    const rowData = employees.employees;
+
+    const defaultColDef = useMemo(() => ({
+        sortable: true,
+        filter: true,
+    }), []);
+
+    const columnDefs = useMemo(
+        () => [
+            { field: "id", width: 70, sortable: true, pinned: 'left' },
+            {
+                field: "fullName",
+                headerName: "Full Name",
+                valueGetter: p => `${p.data.firstName} ${p.data.lastName}`,
+                flex: 1,
+                minWidth: 150,
+                filter: "agTextColumnFilter"
+            },
+            {
+                field: "email",
+                width: 230,
+                cellRenderer: (params) => (
+                    <a
+                        href={`mailto:${params.value}`}
+                        rel="noopener noreferrer"
+                        target="_blank"
+                        className="text-blue-600 hover:underline dark:text-blue-400"
+                    >
+                        {params.value}
+                    </a>
+                )
+            },
+            { field: "department", width: 150 },
+            { field: "position", width: 180 },
+            { field: "location", width: 140 },
+            {
+                field: "salary",
+                headerName: "Salary($)",
+                width: 105,
+                valueFormatter: (p) => `$${p.value.toLocaleString()}`,
+            },
+            { field: "hireDate", width: 120 },
+            {
+                field: "isActive",
+                width: 110,
+                cellRenderer: p =>
+                    p.value
+                        ? <span className='text-green-400 font-semibold'>Active</span>
+                        : <span className='text-red-400 font-semibold'>Inactive</span>
+            },
+            {
+                field: "skills",
+                width: 350,
+                cellRenderer: (p) => (
+                    <div className="flex flex-wrap gap-1">
+                        {p.value.map((skill, index) => (
+                            <span
+                                key={index}
+                                className="px-2 py-[2px] text-xs rounded-md bg-blue-100 text-blue-700 border border-blue-300 my-2"
+                            >
+                                {skill}
+                            </span>
+                        ))}
+                    </div>
+                )
+            },
+            { field: "manager", width: 140 },
+        ],
+        []
+    );
+
+
+    const onFilterTextBoxChanged = (e) => {
+        const value = e.target.value;
+        setFilterText(value);
+
+        if (value === "") {
+            gridRef.current.api.setFilterModel(null);
+        } else {
+            gridRef.current.api.setFilterModel({
+                fullName: {
+                    type: "contains",
+                    filter: value
+                }
+            });
+        }
+    };
+
+
+    return (
+        <div className="px-10">
+
+            {/* Title */}
+            <h1 className="text-[27px] font-bold text-center mb-1">Frontend Dashboard with AG Grid</h1>
+            <p className="text-gray-600 mb-3 text-center">
+                Fetch data from a given file and implement searching, filtering, and sorting.
+            </p>
+
+            {/* Container */}
+            <div className="bg-[#C7C9CE] rounded-xl shadow-xl px-8 py-3 border border-[#F9FAFB] h-155">
+
+                {/* Search Bar */}
+                <div className="mb-3">
+                    <input
+                        type="text"
+                        placeholder="Search by Full Name"
+                        value={filterText}
+                        onChange={onFilterTextBoxChanged}
+                        className="px-3 py-2 w-70 bg-[#F9FAFB] text-black border border-[#F9FAFB] rounded-md focus:ring-2 focus:ring-[rgba(255,255,255,0.4)] outline-none"
+                    />
+                </div>
+
+                {/* Grid */}
+                <div className="ag-theme-alpine-dark w-full h-135">
+                    <AgGridReact
+                        ref={gridRef}
+                        rowData={rowData}
+                        columnDefs={columnDefs}
+                        defaultColDef={defaultColDef}
+                        pagination={true}
+                        paginationPageSize={10}
+                        paginationPageSizeSelector={[10, 20, 50]}
+                        animateRows={true}
+                    />
+                </div>
+
+            </div>
+        </div>
+    );
+}
